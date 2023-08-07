3bb568fd12bcec61e74e985a24fda6b80dae2d11
Sag is the best <87934102+msajo@users.noreply.github.com>
2023-08-07 17:18:23 +1000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,55 @@
+screen_width = 0;
+screen_height = 0;
+
+apple = "";
+speak_data = "";
+to_number = "";
+x = 0;
+y = 0;
+
+draw_apple = "";
+
+var SpeechRecognition = window.webkitSpeechRecognition;
+  
+var recognition = new SpeechRecognition();
+
+function preload(){
+  apple = loadImage(apple.png);
+}
+function start()
+{
+  document.getElementById("status").innerHTML = "System is listening please speak";  
+  recognition.start();
+} 
+ 
+recognition.onresult = function(event) {
+to_number = Number(content);
+ console.log(event); 
+
+ content = event.results[0][0].transcript;
+
+    document.getElementById("status").innerHTML = "The speech has been recognized: " + content; 
+
+}
+
+function setup() {
+ 
+}
+
+function draw() {
+  if(draw_apple == "set")
+  {
+    document.getElementById("status").innerHTML = to_number + " Apples drawn";
+    draw_apple = "";
+  }
+}
+
+function speak(){
+    var synth = window.speechSynthesis;
+
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+
+    synth.speak(utterThis);
+
+    speak_data = "";
+}
